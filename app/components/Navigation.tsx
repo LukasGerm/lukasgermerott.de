56ada90969546197138fc3d7ef07f8941f94ba83
LukasGerm <lukas@lukasgermerott.de>
2021-12-22 13:23:30 +0100

    Close icon in navigation on mobile

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -4,6 +4,54 @@ import Typography from "./Typography";
 import Favicon from "../assets/favicon.png";
 import Divider from "./Divider";
 
+interface IconProps {
+  onClick?(): void;
+}
+
+const BurgerMenuIcon = (props: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="white"
+      className="hover:cursor-pointer"
+      onClick={() => {
+        props.onClick?.();
+      }}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M4 6h16M4 12h16M4 18h16"
+      />
+    </svg>
+  );
+};
+
+const CloseIcon = (props: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="white"
+      className="hover:cursor-pointer"
+      onClick={() => {
+        props.onClick?.();
+      }}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M6 18L18 6M6 6l12 12"
+      />
+    </svg>
+  );
+};
+
 const Navigation = () => {
   const [mobileOpen, setMobileOpen] = useState(false);
 
@@ -21,23 +69,15 @@ const Navigation = () => {
             alt="Lukas Germerott Favicon"
           />
           <div className="h-8 w-8 md:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="white"
-              className="hover:cursor-pointer"
-              onClick={() => {
-                setMobileOpen(!mobileOpen);
-              }}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16M4 18h16"
+            {mobileOpen ? (
+              <CloseIcon onClick={handleClose} />
+            ) : (
+              <BurgerMenuIcon
+                onClick={() => {
+                  setMobileOpen(!mobileOpen);
+                }}
               />
-            </svg>
+            )}
           </div>
         </div>
         <ul className="items-center hidden md:flex">
