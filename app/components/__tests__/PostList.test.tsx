0dfe1fecb8c01ec8e0201b214b14d1064380124d
LukasGerm <lukas@lukasgermerott.de>
2022-02-17 22:02:15 +0100

    Added config for image mocks

--- app/components/__tests__/PostList.test.tsx
+++ app/components/__tests__/PostList.test.tsx
@@ -1,6 +1,7 @@
+import React from "react";
 import { render } from "@testing-library/react";
 import PostList from "../PostList";
-
+jest.mock("../../assets/no_data.svg");
 describe("<PostList />", () => {
   it("Renders nothing found if no post found", async () => {
     const { findByText } = render(<PostList posts={[]} />);
--- jest.config.js
+++ jest.config.js
@@ -23,4 +23,9 @@ module.exports = {
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
+  moduleNameMapper: {
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|less)$": "<rootDir>/__mocks__/fileMock.js",
+  },
 };
